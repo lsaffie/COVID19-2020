375dc5238aa777029c0a90c53b867df0d43de9fa
Luis Saffie <lssaffie@amazon.com>
2020-04-01 23:48:13 -0400

    big refactor

--- app.py
+++ app.py
@@ -15,36 +15,44 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-ConfirmedCases_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-Deaths_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-Recoveries_raw=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+baseURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+
+def loadData(fileName, columnName):
+    data = pd.read_csv(baseURL + fileName) \
+             .drop(['Lat', 'Long'], axis=1) \
+             .melt(id_vars=['Province/State', 'Country/Region'],
+                 var_name='date', value_name=columnName) \
+             .astype({'date':'datetime64[ns]', columnName:'Int64'},
+                 errors='ignore')
+    data['Province/State'].fillna('<all>', inplace=True)
+    data[columnName].fillna(0, inplace=True)
+    return data
+
+ConfirmedCases = loadData(
+    "time_series_covid19_confirmed_global.csv", "Cases")
 
 
 # convert dates into rows
-def cleandata(df_raw):
-    df_cleaned = df_raw.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'], value_name = 'Cases', var_name='Date')
-    df_cleaned = df_cleaned.set_index(['Country/Region', 'Date'])
-    return df_cleaned
-
 
 def ontario_data():
     df = ConfirmedCases[ConfirmedCases['Province/State'] == "Ontario"]
-    df = df.reset_index().drop(columns=['Province/State', 'Lat', 'Long', 'Country/Region'])
+    df = df.reset_index().drop(columns=['Province/State', 'Country/Region'])
     return df
 
 def country_df(country):
-    df = ConfirmedCases.loc[country]
-    df = df.reset_index().drop(columns=['Province/State', 'Lat', 'Long'])
-    return df
+    df = ConfirmedCases[ConfirmedCases['Country/Region'] == country]
+    df = df.reset_index().drop(columns=['Province/State'])
+    return df.groupby('date').max().reset_index()
 
 def df_favs():
-    df_favs = ConfirmedCases.loc["Chile"]
-    df_favs = df_favs.reset_index().drop(columns=['Province/State', 'Lat', 'Long', 'Cases'])
+    df_favs = country_df("Chile")
+    df_favs = df_favs.reset_index().drop(columns=['Cases'])
 
     for c in top_10_countries['Country/Region'].head(7):
-        df_favs[c] = ConfirmedCases.loc[c].reset_index()['Cases']
+        df_favs[c] = country_df(c).reset_index()['Cases']
+        df_favs[c] = country_df(c).reset_index()['Cases']
 
-    return df_favs.set_index('Date')
+    return df_favs.set_index('date')
 
 def plot_timeseries_country(country, title):
     df = country_df(country).tail(30)
@@ -54,26 +62,40 @@ def plot_timeseries_df(df, title, series):
     return html.Div(
             dcc.Graph(
                 id=title + '-graph',
-                figure = px.line(df, x="Date", y="Cases", title=title, text=df[series])),
+                figure = px.line(df, x="date", y="Cases", title=title, text=df[series])),
             )
 
+def plot_ontario_new_cases(column="new cases", title="Ontario New Cases"):
+    return html.Div(
+            dcc.Graph(id=column+'ontario-graph',
+                figure = px.line(df_ontario, x="date", y=column, title=title, text=df_ontario[column]))
+            )
 
-## Clean all datasets
-ConfirmedCases = cleandata(ConfirmedCases_raw)
-DeathCases = cleandata(Deaths_raw)
-RecoveryCases = cleandata(Recoveries_raw)
+def plot_timeseries_canada_province():
+    df = ConfirmedCases[ConfirmedCases['Country/Region'] == "Canada"]
+    df = df.groupby(['date', 'Province/State']).max().reset_index().tail(300)
+    return html.Div(
+            dcc.Graph(
+                id='canada-province-graph',
+                figure = px.line(df, x="date", y="Cases", title="Canada by Province", text=df["Cases"], color="Province/State")),)
+
+def plot_top_countries():
+    return html.Div(
+            dcc.Graph(id='top_countries',
+                figure = px.line(df_top, x="date", y='Cases', title="Top Countries", text=df_top["Cases"], color="Country/Region"))
+            )
 
 df_ontario = ontario_data().tail(30)
 df_chile = country_df("Chile").tail(30)
 df_us= country_df("US").tail(30)
-top_10_countries = ConfirmedCases.max(level=0)['Cases'].reset_index().sort_values('Cases', ascending=False).head(10)
+top_10_countries = ConfirmedCases.groupby('Country/Region').max().sort_values('Cases', ascending=False).head(10).reset_index()
 df_ontario['new cases'] = df_ontario['Cases'].diff()
 df_ontario['new cases3'] = df_ontario['Cases'].diff(3)
 df_favs = df_favs()
 
 ## Top countries
 df_top = ConfirmedCases
-df_top = df_top.reset_index().drop(columns=['Province/State', 'Lat', 'Long'])
+df_top = df_top.reset_index().drop(columns=['Province/State'])
 df_top = df_top[df_top['Country/Region'].isin(
     top_10_countries.head(5)['Country/Region'].to_list()
 )]
@@ -113,13 +135,15 @@ app.layout = html.Div(children=[
 
     plot_timeseries_country("Canada", "Canada Confirmed Cases")
     ,
+    plot_timeseries_canada_province()
+    ,
     plot_timeseries_df(df_ontario, "Ontario Confirmed Cases", "Cases")
     ,
-    plot_timeseries_df(df_ontario, "Ontario Daily New Cases", "new cases")
+    plot_ontario_new_cases()
     ,
-    plot_timeseries_df(df_ontario, "Ontario Daily New Cases (3 days)", "new cases3")
+    plot_ontario_new_cases("new cases3", "Ontario New Cases (3 day aggregation)")
     ,
-    plot_timeseries_df(df_top, "Top Countries", "Cases")
+    plot_top_countries()
     ,
     plot_timeseries_country("US", "US Confirmed Cases")
     ,
